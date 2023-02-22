105848958a522a4b5f1cfbad4cce3fe84425864a
juliancochran <julian.p.cochran@gmail.com>
2023-02-22 13:48:21 -0500

    Commit project files 02.22.203

--- src/APComputerScience.java
+++ src/APComputerScience.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class APComputerScience {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class APComputerScience {
+    private ArrayList<Student> classList;
+
+    public APComputerScience()  {
+        classList = new ArrayList<>();
+    }
+
+    public void addStudent(Student s)   {
+        classList.add(s);
+    }
+
+    public void loadDataFile(String filename)   {
+        try {
+            Scanner in = new Scanner(new File(filename));
+            String[] line;
+            while(in.hasNext()) {
+                line = in.nextLine().split(",");
+            }
+        }
+        catch(Exception e)  {
+            e.printStackTrace();
+        }
+    }
+
+    public void printRoster()   {
+        for(Student s : classList)
+            System.out.println(s);
+        int throwaway = classList.get(0).getAge(true);
+    }
+
+    public static void main(String[] args) {
+        APComputerScience dPeriod = new APComputerScience();
+        dPeriod.addStudent(new Student("Ani", "Sukhija", 12, 18, 4.1, "Julian Cochran"));
+        dPeriod.addStudent(new Student("Issie", "Scurria", 11, 16, 4.05, "Owen Bryant"));
+        dPeriod.addStudent(new Student("Ryan", "Bauroth", 10, 15, 4.23, "Julian Cochran"));
+        dPeriod.addStudent(new Student("Bef", "Steinberg", 11, 18, 4.78, "Julian Cochran"));
+
+        Student s1 = new Student("Shreya", "Rao", 10, 15, 5.23, "Julian Cochran");
+        System.out.println(s1);
+        Student s2 = new Student(s1);
+        System.out.println(s2);
+
+        String word1 = "hello world!";
+        String word2 = new String(word1);
+
+        //dPeriod.printRoster();
+        //dPeriod.printRoster();
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,102 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    // encapsulation of these properties
+    private String firstName, lastName;
+    private int grade, age;
+    private double gpa;
+    private String advisor;
+
+    /**
+     * Default constructor for Student
+     */
+    public Student()    {
+        firstName = "";
+        lastName = new String();
+        grade = 0; age = 0; gpa = 0.;
+        advisor = "";
+    }
+
+    /**
+     *
+     * @param firstName
+     * @param ln
+     * @param gr
+     * @param ag
+     * @param gp
+     * @param adv
+     */
+    public Student(String firstName, String ln, int gr, int ag, double gp, String adv)   {
+        this.firstName = firstName;
+        this.lastName = ln;
+        grade = gr;
+        age = ag;
+        gpa = gp;
+        advisor = adv;
+    }
+
+    /**
+     * This is called a COPY constructor
+     * @param other
+     */
+    public Student(Student other) {
+        this.firstName = new String(other.firstName);
+        this.lastName = other.lastName;
+        this.grade = other.grade;
+        this.age = other.age;
+        this.gpa = other.gpa;
+        this.advisor = other.advisor;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getAge(boolean happyBirthday) {
+        return ++age;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getAdvisor() {
+        return advisor;
+    }
+
+    public String toString()    {
+        return lastName + ", " + firstName + " grade " + grade + " age " + age + " your mom is awesome.";
+    }
+
+    public boolean equals(Object other) {
+        if(other instanceof Student)    {
+            Student temp = (Student)other;
+            boolean same = lastName.equals(temp.lastName) && firstName.equals(temp.firstName);
+            if(same)
+                return this.advisor.equals(temp.advisor);
+            else
+                return same;
+        }
+        else
+            return false;
+    }
 }
+
+
+
+
+
+
+
+
+
